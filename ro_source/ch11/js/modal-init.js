25972b2a5e4f9db4a468003d28023353add81d4e
RolandoDU <rolandolopez3442@yahoo.com>
2016-05-26 09:44:44 -0700

    mid ch11'

---
+++ ro_source/ch11/js/modal-init.js
@@ -0,0 +1,9 @@
+//
+
+(function() {
+    var $content = $('#share-options').detach();
+
+    $('#share').on('click', function() {
+        modal.open({content: $content, width: 340, height: 300});
+    });
+}());
